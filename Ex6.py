c07139144a832cfc8c2dcab92a82f5c8e9511a92
Jayn3D03 <cc220@hawaii.edu>
2025-09-05 12:39:31 -1000

    sentences and weight

---
+++ Ex6.py
@@ -0,0 +1,8 @@
+#ask user to enter weight in pounds
+#convert the weight to kilograms and return the value to the user
+#name: chloe cornforth
+#date: 09/05/2025
+
+weight_in_pounds = input("Please enter your weight in pounds: ")
+weight_in_kilograms = float(weight_in_pounds) * 0.453592
+print(f"You weigh in {weight_in_kilograms} kilograms")
---
+++ Lab2/Ex5.py
@@ -0,0 +1,10 @@
+#ask the user to enter an arbitrary sentence
+#calculate the length of the sentence and return the length to the user
+#name: chloe cornforth
+#date: 09/05/2025
+
+user_sentence = input("Please enter a sentence: ")
+
+sentence_length = len(user_sentence)
+print("You entered:", user_sentence)
+print(f"The length of the sentence you entered is: {sentence_length}")
