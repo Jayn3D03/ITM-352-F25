10b6dc186adec1a1a23406346908b2881c20e66a
Jayn3D03 <cc220@hawaii.edu>
2025-09-24 15:33:05 -1000

    After class edits

--- Lab5/Ex2d.py
+++ Lab5/Ex2d.py
@@ -2,10 +2,10 @@
 # name: chloecornforth
 # date: 9/24/2025
 
-trips = [{' miles ': 1.1, ' duration ': '6.25'},
-        {' miles ': 0.8, ' duration ': '5.25'},
-        {' miles ': 2.5, ' duration ': '10.50'},
-        {' miles ': 2.6, ' duration ': '8.05'}
+trips = [{' miles ': "1.1", ' duration ': "6.25"},
+        {' miles ': "0.8", ' duration ': "5.25"},
+        {' miles ': "2.5", ' duration ': "10.50"},
+        {' miles ': "2.6", ' duration ': "8.05"}
         ]
 
 print(f"the first trip is {trips[0][' miles ']} miles and costs {trips[0][' duration ']}")
---
+++ Lab6/Ex1.py
@@ -0,0 +1,6 @@
+# initialize a tuple with different emotions
+# name: chloecornforth
+# date: 9/24/2025
+
+emotions = ("happy", "sad", "fear", "surprise")
+print(emotions[0] == "happy" and len(emotions) > 3)
